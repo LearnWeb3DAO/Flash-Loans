c5bf29cbf1260703862edc4a6640051e4d8df44f
Haardik <hhaardik@uwaterloo.ca>
2022-05-28 11:38:09 +0530

    update DAI_WHALE address

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Mainnet DAI Address
 const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
 // Address which has a lot of DAI
-const DAI_WHALE = "0xD92B63D0E9F2CE9F77c32BfeB2C6fACd20989eB3";
+const DAI_WHALE = "0xdfD74E3752c187c4BA899756238C76cbEEfa954B";
 
 const POOL_ADDRESS_PROVIDER = "0xa97684ead0e402dc232d5a977953df7ecbab3cdb";
 module.exports = {
